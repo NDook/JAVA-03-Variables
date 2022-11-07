cbd31322ffa45b69dd5718d376a033e5fdd3058e
NDook <thomasmludek@gmail.com>
2022-11-07 12:04:39 +0100

    first commit

---
+++ Variables.java
@@ -0,0 +1,14 @@
+class Variables {
+
+    public static void main(String...args) {
+        String movieTitle = "Indiana Jones et la dernière croisade";
+        boolean seeMovie = true;
+        int year = 1989;
+        double rating = 8.2;
+
+    System.out.println(movieTitle);
+    System.out.println(seeMovie);
+    System.out.println(year);
+    System.out.println(rating);
+    }
+}
